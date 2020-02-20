94d16d654575b4805497f4b4e6757985163348a8
João Vitor S. G <50921035+JojiAbel@users.noreply.github.com>
2020-02-20 11:18:14 -0300

    18-02-20 Study

---
+++ Recursion/18-02/p1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //sum values of X to Y;
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+
+            int Result = Mostra(x, y);
+            Console.WriteLine("resultado = {0}", Result);
+            Console.ReadKey();
+        }
+        static int Mostra(int x, int y)
+        {
+            int result = x;
+            if (x < y)
+            {
+                result = result + Mostra(++x, y);
+            }
+            return result;
+        }
+    }
+}
