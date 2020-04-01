1f153cb5c9253a378c3150714199d45973bb050e
João Vitor S. G <50921035+JojiAbel@users.noreply.github.com>
2020-04-01 11:13:22 -0300

    A simple queue example

---
+++ Queue/SimpleQueue/Simple queue project/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple_queue_project
+{
+    class Student
+    {
+        public string Name;
+        public int studentID;
+    }
+}
